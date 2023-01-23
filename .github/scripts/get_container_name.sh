d81771f92777b6ac106cdbd4d6f8e8071accd73f
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-01-22 22:29:28 -0500

    Change name

--- .github/scripts/get_container_name.sh
+++ .github/scripts/get_container_name.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe
-if [[ -z "${CONTAINER_NAME}" ]]; then
+if [[ -z "${BIOCONDUCTOR_NAME}" ]]; then
   if [[ -z "${TERRA_R_PLATFORM}" ]]; then
-    CONTAINER_NAME="undefined"
+    BIOCONDUCTOR_NAME="undefined"
   else
-    CONTAINER_NAME="${TERRA_R_PLATFORM}"
+    BIOCONDUCTOR_NAME="${TERRA_R_PLATFORM}"
   fi
 fi
 
-echo "${CONTAINER_NAME}"
+echo "${BIOCONDUCTOR_NAME}"
