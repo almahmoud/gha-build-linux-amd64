dfe0a5a01b6b701f3ecb88c88f3521a6257d2571
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-01-31 17:07:46 -0500

    Change check to packages.json

--- .github/scripts/list_tobuild.sh
+++ .github/scripts/list_tobuild.sh
@@ -13,7 +13,7 @@ if [ ! -s tobuild.txt ]; then
       # Add list of packages to build
       grep -lr "readytobuild" lists/ | sed 's#lists/##g' > tobuild.txt
 
-      if [ ! -s tobuild.txt ] && [ -d "lists/" ]; then
+      if [ ! -s tobuild.txt ] && [ -f "packages.json" ]; then
             mkdir -p logs/$(cat runstarttime)
             counter=0
             if [ -f "logs/$(cat runstarttime)/retries_counter" ]; then
